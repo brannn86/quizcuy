3731b0f0cdc570205c1fcafac84f0841d1785927
brannn86 <gibran.hattami@gmail.com>
2023-12-01 19:26:27 +0700

    Passing quiz data to DetailsFragment

--- app/src/main/java/com/brafly/quizcuy/views/DetailsFragment.java
+++ app/src/main/java/com/brafly/quizcuy/views/DetailsFragment.java
@@ -2,65 +2,102 @@ package com.brafly.quizcuy.views;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.brafly.quizcuy.R;
+import com.brafly.quizcuy.model.QuizListModel;
+import com.brafly.quizcuy.viewmodel.QuizListViewModel;
+import com.bumptech.glide.Glide;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
 public class DetailsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DetailsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DetailsFragment newInstance(String param1, String param2) {
-        DetailsFragment fragment = new DetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private TextView titleDetail, diff_num, questions_num;
+    private Button start_btn;
+    private NavController navController;
+    private int position;
+    private ProgressBar progressBar;
+    private QuizListViewModel viewModel;
+    private ImageView imgDetail;
+    private String quizID;
+    private long totalQueCount;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory
+                .getInstance(getActivity().getApplication())).get(QuizListViewModel.class);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        titleDetail = view.findViewById(R.id.titleDetail);
+        diff_num = view.findViewById(R.id.diff_num);
+        questions_num = view.findViewById(R.id.questions_num);
+        start_btn = view.findViewById(R.id.start_btn);
+        progressBar = view.findViewById(R.id.detailBar);
+        imgDetail = view.findViewById(R.id.imgDetail);
+
+        navController = Navigation.findNavController(view);
+
+        position = DetailsFragmentArgs.fromBundle(getArguments()).getPosition();
+
+        viewModel.getQuizListLiveData().observe(getViewLifecycleOwner(), new Observer<List<QuizListModel>>() {
+            @Override
+            public void onChanged(List<QuizListModel> quizListModels) {
+                QuizListModel quiz = quizListModels.get(position);
+                diff_num.setText(String.valueOf(quiz.getDifficulty()));
+                titleDetail.setText(quiz.getTitle());
+                questions_num.setText(String.valueOf(quiz.getQuestions()));
+                Glide.with(view).load(quiz.getImage()).into(imgDetail);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }, 2000);
+                totalQueCount = quiz.getQuestions();
+                quizID = quiz.getQuizId();
+            }
+        });
+
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DetailsFragmentDirections.ActionDetailsFragmentToQuizFragment action =
+                        DetailsFragmentDirections.actionDetailsFragmentToQuizFragment();
+
+                action.setQuizID(quizID);
+                action.setTotalQueCount(totalQueCount);
+                navController.navigate(action);
+
+            }
+        });
     }
 }
